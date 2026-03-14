b3b7bf2e0fed34f05cfa1a45822d23ffaf223d7d
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2026-03-14 11:58:47 -0500

    Actualización

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,22 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    componenteFormativo: 'Herramientas informáticas en las organizaciones',
+    descripcionCurso:
+      'Este componente formativo tiene como fin dar a conocer cada una de las herramientas informáticas que se requieren para el óptimo manejo de una organización.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.png'),
       },
       {
-        clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        clases: ['banner-principal-decorativo-2', 'd-none', 'd-lg-block'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.png'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3', 'd-none', 'd-lg-block'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.png'),
       },
     ],
   },
@@ -31,28 +36,43 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Sistemas informáticos en una organización',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Herramientas informáticas para la organización',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Funcionalidades de los dispositivos informáticos',
+            hash: 't_1_2',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Implementación de componentes',
         desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Tema 3',
-        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Instalación de herramientas informáticas',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Búsqueda de información técnica',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Gestión de cuentas corporativas de sistemas informáticos',
+            hash: 't_2_3',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -86,7 +106,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/233108_CF01_DU.zip',
       },
       {
         icono: 'fas fa-download',
@@ -102,22 +122,115 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: 'Sitio web',
-      link: '',
+      tema: 'Sistemas informáticos en una organización',
+      referencia:
+        'Prieto, A., Martínez, M. (2004). <i>Sistemas de información en las organizaciones: Una alternativa para mejorar la productividad gerencial en las pequeñas y medianas empresas</i>. Revista de Ciencias Sociales (RCS) Vol. X, No. 2, pp. 322 - 337 FACES - LUZ · ISSN 1315-9518.',
+      tipo: 'Artículo',
+      link: 'https://www.redalyc.org/pdf/280/28010209.pdf',
+    },
+    {
+      tema: 'Herramientas informáticas para la organización',
+      referencia:
+        'Bautista, L., Abreu, M. (2022). <i>El trabajo colaborativo desde la virtualidad a través de las herramientas tecnológicas. In Libro de Actas del 2. º Congreso Caribeño de Investigación Educativa: Nuevos paradigmas y experiencias emergentes (pp. 577-581)</i>. Instituto Superior de Formación Docente Salomé Ureña (ISFODOSU).',
+      tipo: 'Libro',
+      link: 'https://dialnet.unirioja.es/servlet/articulo?codigo=8498392',
+    },
+    {
+      tema: 'Funcionalidades de los dispositivos informáticos',
+      referencia:
+        'Hernández, A (s.f). <i>Los sistemas de información: evolución y desarrollo</i>. departamento de Economía y Dirección de Empresas Universidad de Zaragoza.',
+      tipo: 'Artículo',
+      link: 'https://dialnet.unirioja.es/descarga/articulo/6255073.pdf',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: '<i>Software</i>',
+      significado:
+        'programas de computadora, instrucciones o procedimientos que se ejecutan para operar y satisfacer las necesidades específicas de un usuario.',
+    },
+    {
+      termino: 'Bases de datos',
+      significado:
+        'conjunto ordenado de datos personales referentes a una persona física identificada o identificable, condicionados a criterios determinados, con independencia de la forma o modalidad de su creación, tipo de soporte, procesamiento, almacenamiento y organización.',
+    },
+    {
+      termino: 'Código abierto',
+      significado:
+        'se refiere al código fuente del <i>software</i> que es abiertamente accesible y que puede ser cambiado y distribuido por cualquier persona.',
+    },
+    {
+      termino: 'Desarrollo tecnológico',
+      significado:
+        'trabajo sistemático fundamentado en los conocimientos obtenidos por la investigación o la experiencia práctica, que se dirige a la fabricación de nuevos materiales, productos o dispositivos; a establecer nuevos procedimientos, sistemas y servicios, o a mejorar considerablemente los ya existentes.',
+    },
+    {
+      termino: 'Equipos de cómputo',
+      significado:
+        'dispositivo electrónico que almacena y procesa información para después mostrarla en una interfaz a la disposición del usuario, permite una interacción del <i>hardware</i> (parte tangible) con el software (parte intangible).',
+    },
+    {
+      termino: '<i>Hardware</i>',
+      significado:
+        'conjunto de elementos físicos o materiales que constituyen una computadora o un sistema informático.',
+    },
+    {
+      termino: 'Licencia de <i>software</i>',
+      significado:
+        'contrato en donde el usuario acepta los términos y condiciones del fabricante para poder hacer uso del <i>software</i>.',
+    },
+    {
+      termino: 'Mantenimiento de <i>software</i>',
+      significado:
+        'es la modificación de un producto de software después de la entrega, para corregir errores, mejorar el rendimiento, u otros atributos.',
+    },
+    {
+      termino: 'Producto de <i>software</i>',
+      significado:
+        'se refiere al sistema (código fuente), documentación asociada y datos necesarios para el funcionamiento del <i>software</i> que se desarrolló y que se entrega a un cliente o usuario.',
+    },
+    {
+      termino: 'Pruebas de <i>software</i>',
+      significado:
+        'proceso de analizar un componente de <i>software</i> u operar un sistema que ayuda a detectar errores o defectos y a identificar la completitud o diferencias con respecto a los requerimientos y acuerdos establecidos con la finalidad de evaluar la calidad del <i>software</i>.',
+    },
+    {
+      termino: 'Redes de Computadores',
+      significado:
+        'también llamada red de ordenadores o red informática. Es un conjunto de equipos conectados por medio de cables, señales, ondas o cualquier otro método de transporte de datos, que comparten información (archivos), recursos (CD-ROM, impresoras, etc.)',
+    },
+    {
+      termino: 'Seguridad de la información',
+      significado:
+        'la capacidad de preservar la confidencialidad, integridad y disponibilidad de la información, así como la autenticidad, confiabilidad, trazabilidad y no repudio de la misma.',
+    },
+    {
+      termino: 'Servicio de autenticación',
+      significado:
+        'servicio de seguridad que verifica la identidad alegada por una entidad.',
+    },
+    {
+      termino: 'Sistema de información',
+      significado:
+        'conjunto de aplicaciones, servicios, activos de tecnología de la información u otros componentes que manejan información.',
+    },
+    {
+      termino: 'Sistema Operativo',
+      significado:
+        'un sistema operativo es el <i>software</i> o programa más importante que se ejecuta en un computador, nos permite usarlo y darle órdenes para que haga lo que necesitamos.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Las mejores herramientas de software para la gestión de organizaciones. (2021) ',
       link: '',
+    },
+    {
+      referencia:
+        'Salem Al-Mamary, Yaser Hasan & Shamsuddin, Alina & Aziati, A. ResearchGate. El papel de los diferentes tipos de sistemas de información en las organizaciones empresariales. (2019). ',
+      link:
+        'https://www.researchgate.net/publication/264556488_The_Role_of_Different_Types_of_Information_Systems_In_Business_Organizations_A_Review',
     },
   ],
   creditos: [
@@ -125,14 +238,15 @@ export default {
       titulo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre completo',
-          cargo: 'Responsable del ecosistema',
+          nombre: 'Claudia Patricia Aristizábal Gutiérrez',
+          cargo: 'Responsable del equipo',
           centro: 'Dirección General',
         },
         {
-          nombre: 'Nombre completo',
+          nombre: 'Liliana Victoria Morales Gualdrón',
           cargo: 'Responsable de línea de producción',
-          centro: 'Centro XYZ - Regional XYZ',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -140,9 +254,44 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Ronald Alexander Vacca Ascanio',
+          cargo: 'Experto temático',
+          centro:
+            'Regional Norte de Santander - Centro de la industria, la empresa y los servicios',
+        },
+        {
+          nombre: 'Jeimy Lorena Romero Perilla',
+          cargo: 'Diseño instruccional',
+          centro:
+            'Regional Norte de Santander - Centro de la industria, la empresa y los servicios',
+        },
+        {
+          nombre: 'Silvia Milena Sequeda Cárdenas',
+          cargo: 'Asesoría metodológica y pedagógica',
+          centro: 'Regional Distrito Capital - Centro de Diseño y Metrología',
+        },
+        {
+          nombre: 'Rafael Neftalí Lizcano Reyes',
+          cargo: 'Responsable equipo de desarrollo curricular',
+          centro:
+            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+        },
+        {
+          nombre: 'Sandra Patricia Hoyos Sepúlveda',
+          cargo: 'Corrección de estilo',
+          centro: 'Regional Distrito Capital - Centro de Diseño y Metrología',
+        },
+        {
+          nombre: 'Gloria Lida Alzate Suarez',
+          cargo: 'Adecuación instruccional',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
+        },
+        {
+          nombre: 'Alix Cecilia Chinchilla Rueda',
+          cargo: 'Metodología para la formación virtual',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -150,19 +299,34 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador de contenidos',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Jesús Antonio Vecino Valero',
+          cargo: 'Diseñador web',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Desarrollador <i>full stack</i>',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Jhon Jairo Urueta Álvarez',
+          cargo: 'Desarrollo fullstack',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Animador y productor audiovisual',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Lady Adriana Ariza Luque',
+          cargo: 'Animación y producción audiovisual',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
+        },
+        {
+          nombre: 'Laura Gisselle Murcia Pardo',
+          cargo: 'Animación y producción audiovisual',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
+        },
+        {
+          nombre: 'Ernesto Navarro Jaimes',
+          cargo: 'Animación y producción audiovisual',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -170,14 +334,23 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Validador y vinculador de recursos educativos digitales',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Carolina Coca Salazar',
+          cargo: 'Evaluación de contenidos inclusivos y accesibles',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Evaluador de contenidos inclusivos y accesibles',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Lina Marcela Pérez Manchego',
+          cargo: 'Validación de recursos educativos digitales',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
+        },
+        {
+          nombre: 'Leyson Fabian Castaño Pérez',
+          cargo:
+            'Validación de recursos educativos digitales y vinculación LMS',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -188,4 +361,25 @@ export default {
     creativeCommons:
       'Licencia creative commons CC BY-NC-SA<br><a href="https://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">ver licencia</a>',
   },
+  // creditosInicio: [
+  //   {
+  //     titulo: 'En alianza',
+  //     contenido: [
+  //       require('@/assets/template/logo-sena-naranja.svg'),
+  //       require('@/assets/template/mintic.jpg'),
+  //       require('@/assets/template/minsalud.jpg'),
+  //     ],
+  //   },
+  //   {
+  //     titulo: 'En compañía',
+  //     contenido: [
+  //       require('@/assets/template/presidencia.jpg'),
+  //       require('@/assets/template/ecopetrol.jpg'),
+  //     ],
+  //   },
+  //   {
+  //     titulo: 'Una iniciativa',
+  //     contenido: [require('@/assets/template/santander.jpg')],
+  //   },
+  // ],
 }
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,15 +2,17 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum.
+
+    p El presente componente formativo da a conocer las principales herramientas informáticas que se requieren actualmente para el óptimo funcionamiento de una organización, pues es innegable que las tecnologías de la información han cambiado la forma de hacer las cosas, por ende, es necesario adquirir los conocimientos necesarios para poder aplicarlas de manera eficaz en cualquier contexto del ser humano. 
+    p.mb-5 Una breve revisión de los temas vistos, se encuentran en el siguiente esquema:
     .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+      .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="A través de esta síntesis se resume los temas abordados en el presente componente formativo como es la definición y sus principales características del: Software, Hardware, Computación en la nube y Búsqueda de información.")
       .col-auto
-        a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
+        a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
-            img(src="@/assets/bullets/icono-pdf.svg")
+            img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
             p Anexo. Síntesis
 
